cbd0bce8ceaa91e769e545328bef9af842caaf6e
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2020-01-05 23:16:55 +0530

    Mandatory Hello World

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:gotta_learn_dart/gotta_learn_dart.dart' as gotta_learn_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gotta_learn_dart.calculate()}!');
+}
---
+++ test/gotta_learn_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:gotta_learn_dart/gotta_learn_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
