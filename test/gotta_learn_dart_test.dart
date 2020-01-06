9114a1cb16c724d916a0269b0873d4771c76bd2e
FlashBlaze <neerajlagwankar@gmail.com>
2020-01-06 18:25:33 +0530

    Tests

--- test/gotta_learn_dart_test.dart
+++ test/gotta_learn_dart_test.dart
@@ -2,7 +2,10 @@ import 'package:gotta_learn_dart/gotta_learn_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Testing Fibonacci series', () {
+    expect(fib(0), equals(0));
+    expect(fib(2), equals(1));
+    expect(fib(8), equals(21));
+    expect(fib(20), equals(6765));
   });
 }
